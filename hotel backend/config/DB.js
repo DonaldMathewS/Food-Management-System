ba9eae98cc860230787e30e814fe6b1a2a3ab679
Doanld Mathew S <srdonald111@gmail.com>
2025-03-21 15:36:55 +0530

    Update DB.js
    MONGO DB URLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLK

--- hotel backend/config/DB.js
+++ hotel backend/config/DB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 exports.connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://srdonald111:admin@cluster0.a4rfg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "YOUR MANGO DB URL"
     )
     .then(() => {
       console.log(" DB connected ");
